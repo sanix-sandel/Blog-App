ec923a400d1339252e1f995be12cba04b1c058fc
sanix sandel <sanicksikani@gmail.com>
2020-04-25 01:42:20 +0300

    chapter2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,11 +4,19 @@ from django.core.paginator import (
 )
 from django.core.mail import send_mail
 from django.views.generic import ListView 
-from .forms import EmailPostForm
-from .models import Post
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
+from taggit.models import Tag
+from django.db.models import Count
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list=Post.published.all()
+    
+    tag=None
+    if tag_slug:
+        tag=get_object_or_404(Tag, slug=tag_slug)
+        object_list=object_list.filter(tags__in=[tag])
+
     paginator=Paginator(object_list, 3)
     page=request.GET.get('page')
     try:
@@ -20,7 +28,9 @@ def post_list(request):
         #if page is out of range deliver last page of results    
         posts=paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html',
-                {'page':page, 'posts':posts})
+                {'page':page, 'posts':posts, 'tag':tag})
+
+
 
 class PostListView(ListView):
     queryset=Post.published.all()
@@ -36,8 +46,29 @@ def post_detail(request, year, month, day, post):
                                     publish__year=year,
                                     publish__month=month,
                                     publish__day=day)
+    comments=post.comments.filter(active=True)
+    new_comment=None
+
+    if request.method=='POST':
+
+        comment_form=CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment=comment_form.save(commit=False)
+            new_comment.post=post
+            new_comment.save()
+    else:
+            comment_form=CommentForm()
+    
+    post_tags_ids=post.tags.values_list('id', flat=True)
+    similar_posts=Post.published.filter(tags__in=post_tags_ids)\
+                                        .exclude(id=post.id)
+    similar_posts=similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags', '-publish')[:4]                                    
     return render(request, 'blog/post/detail.html',
-                    {'post':post})                                                
+                    {'post':post, 'comments':comments,
+                    'new_comment':new_comment,
+                    'comment_form':comment_form,
+                    'similar_posts':similar_posts})                                                
 
 def post_share(request, post_id):
     post=get_object_or_404(Post, id=post_id, status='published')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'blog.apps.BlogConfig',
+    'taggit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
