0d1902e778cce5426375952ca6fcad9c9b397950
sanix sandel <sanicksikani@gmail.com>
2020-04-24 21:49:39 +0300

    sending emails

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import (
     Paginator, EmptyPage, PageNotAnInteger
 )
+from django.core.mail import send_mail
 from django.views.generic import ListView 
+from .forms import EmailPostForm
 from .models import Post
-
-
 
 def post_list(request):
     object_list=Post.published.all()
@@ -38,3 +38,29 @@ def post_detail(request, year, month, day, post):
                                     publish__day=day)
     return render(request, 'blog/post/detail.html',
                     {'post':post})                                                
+
+def post_share(request, post_id):
+    post=get_object_or_404(Post, id=post_id, status='published')
+    sent=False
+    if request.method=='POST':
+        form=EmailPostForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+
+            post_url=request.build_absolute_uri(
+                post.get_absolute_url())
+            subject=f"{cd['name']} recommends you read {post.title}"
+
+            message=f"Read {post.title} at {post_url}\n\n" \
+                    f"{cd['name']}\'s comments:{cd['comments']}"
+
+            send_mail=(subject, message, 'techyintelo@gmail.com', [cd['to']])
+    else:
+        form=EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post':post,
+                                                    'form':form})          
+
+
+#If your form data does not validate,
+#cleaned_data will contain
+#only the valid fields.                                                                          
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='techyintelo@gmail.com'
+EMAIL_HOST_PASSWORD='19972017Russia'
+EMAIL_USE_TLS=True
